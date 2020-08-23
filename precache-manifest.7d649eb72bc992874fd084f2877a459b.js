552d0b45ad4ca9293bcba8f70fe689566233f844
luojinxu <7752998+LunaLuo@user.noreply.gitee.com>
2020-08-23 08:58:10 +0800

    deploy

---
+++ precache-manifest.7d649eb72bc992874fd084f2877a459b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d40daed681af57d2ff0",
+    "url": "/orangeAccount/css/app.bc01499e.css"
+  },
+  {
+    "revision": "e8403c2cd854da0497e6ad308866aab3",
+    "url": "/orangeAccount/index.html"
+  },
+  {
+    "revision": "4d40daed681af57d2ff0",
+    "url": "/orangeAccount/js/app.d8a31266.js"
+  },
+  {
+    "revision": "579f8f1ca78f218c1e2e",
+    "url": "/orangeAccount/js/chunk-vendors.2862e9b6.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/orangeAccount/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/orangeAccount/robots.txt"
+  }
+]);
